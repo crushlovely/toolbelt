e5631edc86d87013828757bf20823ad25b818af7
PJ Kelly <pj@crushlovely.com>
2014-07-31 20:12:24 -0700

    Setting prerelease version.

--- lib/toolbelt/version.rb
+++ lib/toolbelt/version.rb
@@ -1,3 +1,3 @@
 module Toolbelt
-  VERSION = '0.1.0'
+  VERSION = '0.0.1.pre'
 end
--- spec/lib/toolbelt/version_spec.rb
+++ spec/lib/toolbelt/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'Toolbelt::VERSION' do
   it 'should be the correct version' do
-    expect(Toolbelt::VERSION).to eq('0.1.0')
+    expect(Toolbelt::VERSION).to eq('0.0.1.pre')
   end
 end
